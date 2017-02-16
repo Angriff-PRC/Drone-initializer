afb4c0e759563eafa7f7dbec1c5d0db17b9cdbbe
Angriff-PRC <2835911331@qq.com>
2017-02-16 16:28:29 +0800

    Add files via upload

---
+++ 初始化程序.py
@@ -0,0 +1,36 @@
+class drone:
+          def __init__(self):
+                    self.__on  = True
+                    slef.off = False
+                    self.__height = 0
+                    self.__speed = 0
+                    self.__location = [0,0]
+
+          def Turn_on(self):
+                    return self.__on
+
+          def Turn_off(self):
+                    return self.__off
+
+          def get_height(self):
+                    return self.__height
+
+          def set_height(self):
+                    self.__height = height
+
+          def get_speed(self):
+                    return self.__speed
+
+          def set_speed(self):
+                    self.__speed = speed
+
+          def set_location(self):
+                    self.__location = location
+
+          def get_location(self):
+                    return self.__location
+
+          
+                    
+                    
+     
